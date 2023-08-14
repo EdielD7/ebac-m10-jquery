e7762918dccbb19e70c6e4513fa4c0936387691b
EdielD7 <129358065+EdielD7@users.noreply.github.com>
2023-08-14 13:02:17 -0400

    feat(validation): add styled validation

--- script.js
+++ script.js
@@ -2,4 +2,28 @@ $(document).ready(function() {
     $('#telefone').mask('(00) 0 0000-0000'),
     $('#cpf').mask('000.000.000-00'),
     $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        }
+    })
 })
